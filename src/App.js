7732ae9ab1c2a6d09f973c11e01a8a4bd9b39d08
igor-kolyaskin <131876836+igor-kolyaskin@users.noreply.github.com>
2023-05-30 16:34:58 +0400

    refactor: implement nested routing wiht Outlet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./components/routes/home/home.component";
 
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the nav bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+
+const Shop = () => {
+  return <div>I'm a Shop page</div>;
+};
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
