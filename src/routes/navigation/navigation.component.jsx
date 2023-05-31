30e9edeeeaf68dccaf71d09763e6d512f2cda1a0
igor-kolyaskin <131876836+igor-kolyaskin@users.noreply.github.com>
2023-05-31 08:47:03 +0400

    feat: add sign-in link

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -10,6 +10,7 @@ const Navigation = () => {
                 <Link className="logo-container" to="/"><CrwnLogo className="logo" /></Link>
                 <div className="nav-links-container">
                     <Link className="nav-link" to="shop">SHOP</Link>
+                    <Link className="nav-link" to="sign-in">SIGN IN</Link>
                 </div>
             </div>
             <Outlet />
