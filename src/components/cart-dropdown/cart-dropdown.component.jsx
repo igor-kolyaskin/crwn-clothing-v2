0e0285c74046d4eac4f52bd7c4c323e466b12bce
igor-kolyaskin <131876836+igor-kolyaskin@users.noreply.github.com>
2023-07-01 19:03:42 +0400

    style: change CartDropdown styling to styled-component with child element's styling

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,7 +3,11 @@ import { CartContext } from '../../contexts/cart.context';
 import { useNavigate } from 'react-router-dom';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
-import './cart-dropdown.styles.scss';
+import {
+    CartDropdownContainer,
+    EmptyMessage,
+    CartItems
+} from './cart-dropdown.styles';
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
@@ -14,12 +18,14 @@ const CartDropdown = () => {
     };
 
     return (
-        <div className='cart-dropdown-container'>
-            <div className="cart-items">
-                {cartItems.map(item => <CartItem cartItem={item} key={item.id} />)}
-            </div>
+        <CartDropdownContainer>
+            <CartItems>
+                {cartItems.length
+                    ? cartItems.map(item => <CartItem cartItem={item} key={item.id} />)
+                    : <EmptyMessage>Your cart is empty</EmptyMessage>}
+            </CartItems>
             <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-        </div>
+        </CartDropdownContainer>
     );
 };
 
--- src/components/cart-dropdown/cart-dropdown.styles.jsx
+++ src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -1,4 +1,12 @@
-.cart-dropdown-container {
+import styled from 'styled-components';
+
+import {
+    BaseButton,
+    GoogleSingInButton,
+    InvertedButton
+} from '../button/button.styles';
+
+export const CartDropdownContainer = styled.div`
     position: absolute;
     width: 240px;
     height: 340px;
@@ -11,19 +19,21 @@
     right: 40px;
     z-index: 5;
 
-    .empty-message {
+    ${BaseButton},
+    ${GoogleSingInButton},
+    ${InvertedButton} {
+        margin-top: auto;
+    }
+`;
+
+export const EmptyMessage = styled.span`
         font-size: 18px;
         margin: 50px auto;
-    }
+`;
 
-    .cart-items {
+export const CartItems = styled.div`
         height: 240px;
         display: flex;
         flex-direction: column;
         overflow: scroll;
-    }
-
-    button {
-        margin-top: auto;
-    }
-}
+`;
