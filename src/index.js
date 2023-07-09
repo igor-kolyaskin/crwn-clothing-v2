6bd2402aede0fcdcedfcc831a813afafc8978b66
igor-kolyaskin <131876836+igor-kolyaskin@users.noreply.github.com>
2023-07-09 09:48:27 +0400

    feat: implement persist storage with redux-persist lib

--- src/index.js
+++ src/index.js
@@ -4,16 +4,18 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 import { Provider } from 'react-redux';
-import { store } from './store/store';
+import { persistor, store } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-    ,
   </React.StrictMode>,
   document.getElementById('root')
 );
