8414e3483d57d285846f166dcfe99a376f0f2b94
igor-kolyaskin <131876836+igor-kolyaskin@users.noreply.github.com>
2023-07-19 08:54:58 +0400

    init: configure store

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+// eslint-disable-next-line no-unused-vars
 import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
+// eslint-disable-next-line no-unused-vars
 import { store, persistor } from './store/store';
 
 import './index.scss';
@@ -14,11 +16,11 @@ const rootElement = document.getElementById('root');
 render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </PersistGate>
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>,
   rootElement
