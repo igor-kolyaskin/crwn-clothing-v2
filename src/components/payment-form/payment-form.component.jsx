e3ef0224a3c8c05b7bab742d1ee8845b0d6fb764
igor-kolyaskin <131876836+igor-kolyaskin@users.noreply.github.com>
2023-07-25 10:38:48 +0400

    feat: implement payment form and serverless function for Stripe

---
+++ src/components/payment-form/payment-form.component.jsx
@@ -0,0 +1,29 @@
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
+import { FormContainer, PaymentFormContainer } from './payment-form.styles';
+
+const PaymentForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    // eslint-disable-next-line no-unused-vars
+    const paymentHandler = async (e) => {
+        e.preventDefault();
+
+        if(!stripe || !elements) return;
+        
+
+    };
+
+    return (
+        <PaymentFormContainer>
+            <FormContainer>
+                <h2>Credit Card Payment: </h2>
+                <CardElement />
+                <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
+            </FormContainer>
+        </PaymentFormContainer>
+    );
+};
+
+export default PaymentForm;
