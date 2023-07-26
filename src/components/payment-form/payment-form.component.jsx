05000a4b52a92f83db8062c07105188af5e50d68
igor-kolyaskin <131876836+igor-kolyaskin@users.noreply.github.com>
2023-07-26 17:08:38 +0400

    feat: finish payment button improvement

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,29 +1,68 @@
-import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
-import { FormContainer, PaymentFormContainer } from './payment-form.styles';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js'
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component'
+import { FormContainer, PaymentFormContainer } from './payment-form.styles'
+import { useSelector } from 'react-redux'
+import { selectCartTotal } from '../../store/cart/cart.selector'
+import { selectCurrentUser } from '../../store/user/user.selector'
+import { useState } from 'react'
 
 const PaymentForm = () => {
-    const stripe = useStripe();
-    const elements = useElements();
+    const stripe = useStripe()
+    const elements = useElements()
+    const amount = useSelector(selectCartTotal)
+    const currentUser = useSelector(selectCurrentUser)
+    const [isProcessingPayment, setIsProcessingPayment] = useState(false)
 
     // eslint-disable-next-line no-unused-vars
     const paymentHandler = async (e) => {
-        e.preventDefault();
+        e.preventDefault()
 
-        if(!stripe || !elements) return;
-        
+        if (!stripe || !elements) return
+        setIsProcessingPayment(true)
 
-    };
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ amount: amount * 100 })
+        }).then(res => res.json())
+
+        const { paymentIntent: { client_secret } } = response
+        console.log(client_secret)
+
+        const paymentResult = await stripe.confirmCardPayment(client_secret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: currentUser ? currentUser.displayName : 'guest'
+                }
+            }
+        })
+
+        setIsProcessingPayment(false)
+
+        if (paymentResult.error) {
+            alert(paymentResult.error)
+        } else {
+            if (paymentResult.paymentIntent.status === 'succeeded') {
+                alert('Payment successfull')
+            }
+        }
+    }
 
     return (
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer onSubmit={paymentHandler}>
                 <h2>Credit Card Payment: </h2>
                 <CardElement />
-                <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
+                <Button
+                    buttonType={BUTTON_TYPE_CLASSES.inverted}
+                    isLoading={isProcessingPayment}
+                >Pay now</Button>
             </FormContainer>
         </PaymentFormContainer>
-    );
-};
+    )
+}
 
-export default PaymentForm;
+export default PaymentForm
